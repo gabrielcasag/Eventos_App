c3c78ed3880e58d8f7c0ce234f4bc06f692a1114
Gabriel Casagrande <gaspericasag@gmail.com>
2020-06-01 22:05:32 -0300

    Inicializado o projeto

---
+++ src/test/java/com/gabrielcasag/appeventos/AppeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcasag.appeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
